a24ba92f4a6be8e45f948dc2b5b7bd8475462668
Sarah Flynn <sarah.flynn@icloud.com>
2018-11-21 14:57:17 -0800

    app title displaying to UI

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+
+export default function App() {
+    return (
+        <Fragment>
+            <h1>Parking Lot</h1>
+        </Fragment>
+    )
+}
