43e2017a921bcc90496ce8903629d035ac806fe5
KLPainter <KLPainter@gmail.com>
2018-11-23 22:08:03 -0800

    working on lot reducers

--- src/reducers/lot.js
+++ src/reducers/lot.js
@@ -1,3 +1,5 @@
+import { UPDATE_LOT_SEARCH_TERM, LOT_CAR_ARRIVED, LOT_CAR_DEPARTED } from '../actions/lot';
+
 const initialState = {
   searchTerm: '',
   list: ['ABC123', 'YCU411']
@@ -5,6 +7,12 @@ const initialState = {
 
 export default function reducer(state = initialState, { type, payload }) {
   switch(type) {
+    case UPDATE_LOT_SEARCH_TERM:
+      return { ...state, searchTerm: payload };
+    case LOT_CAR_ARRIVED:
+      return { ...state, list: [...state.list, payload] };
+    case LOT_CAR_DEPARTED:
+      return { ...state, list: state.list.splice(state.list.indexOf(payload), 1) };
     default:
       return state;
   }
--- src/reducers/lotUse.js
+++ src/reducers/lotUse.js
@@ -1,3 +1,5 @@
+import { LOT_CAR_ARRIVED } from '../actions/lot';
+
 const initialState = {
   'ABC123': 1,
   'YCU411': 2,
@@ -6,6 +8,8 @@ const initialState = {
 
 export default function reducer(state = initialState, { type, payload }) {
   switch(type) {
+    case LOT_CAR_ARRIVED:
+      return { ...state, state[payload]: state[payload]++}
     default:
       return state;
   }
