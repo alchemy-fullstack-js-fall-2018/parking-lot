2fd2c7bdc7db2686ba18e94bb320f5eb4f014e53
KLPainter <KLPainter@gmail.com>
2018-11-26 12:15:35 -0800

    split car registration (for no reason)

---
+++ src/components/CarRegistration/CarRegistration.js
@@ -0,0 +1,60 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import ROUTES from '../../routes';
+
+
+export default class CarRegistration extends PureComponent {
+
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired
+  };
+
+  state = {
+    plate: '',
+    make: '',
+    model: '',
+    color: ''
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  };
+
+  handleSubmit = event => {
+    const { plate, make, model, color } = this.state;
+    event.preventDefault();
+    this.props.onSubmit({ plate, make, model, color });
+    this.props.history.push(ROUTES.CAR.linkTo(plate));
+  };
+
+  render() {
+    const { plate, make, model, color } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>New Registration</h2>
+
+        <p>
+          <label htmlFor="plate">Plate: </label>
+          <input type="text" name="plate" value={plate} onChange={this.handleChange} />
+        </p>
+
+        <p>
+          <label htmlFor="make">Make: </label>
+          <input type="text" name="make" value={make} onChange={this.handleChange} />
+        </p>
+
+        <p>
+          <label htmlFor="model">Model: </label>
+          <input type="text" name="model" value={model} onChange={this.handleChange} />
+        </p>
+
+        <p>
+          <label htmlFor="color">Color: </label>
+          <input type="text" name="color" value={color} onChange={this.handleChange} />
+        </p>
+
+        <button type="submit">Register</button>
+      </form>
+    );
+  }
+}
