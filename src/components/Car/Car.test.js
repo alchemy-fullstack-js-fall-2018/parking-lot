2cae84dfef98715e94766e7d299816c7e1e4408b
KLPainter <KLPainter@gmail.com>
2018-11-26 09:55:54 -0800

    Car component test passing

---
+++ src/components/Car/Car.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Car from './Car';
+
+describe ('tests for Car component', () => {
+
+  it('renders an Car component', () => {
+    const car = {
+      plate: 'ZZZ000',
+      make: 'Plymouth',
+      model:  'Edsel',
+      color: 'pink'
+    };
+    const wrapper = shallow(<Car car={car}/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+});
